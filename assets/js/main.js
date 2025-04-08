ab4e389073be27778ed49307175887f1feb5c861
Solvebytez <githubsolvebytez@gmail.com>
2025-04-08 14:26:42 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,73 @@ $(document).ready(function () {
         }
     });
 });
+//Scroll Percentage
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollPercent = document.querySelector(".scroll-count-percent");
+    const progressCircle = document.querySelector(".progress__value");
+    const percentText = document.querySelector(".percent-number-text");
+    const radius = progressCircle.r.baseVal.value;
+    const circumference = 2 * Math.PI * radius;
+  
+    progressCircle.style.strokeDasharray = `${circumference}`;
+    progressCircle.style.strokeDashoffset = `${circumference}`;
+  
+    function setProgress(percent) {
+      const offset = circumference - (percent / 100) * circumference;
+      progressCircle.style.strokeDashoffset = offset;
+      percentText.textContent = Math.round(percent);
+    }
+  
+    window.addEventListener("scroll", function () {
+      const scrollTop = window.scrollY;
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const scrollPercentValue = (scrollTop / docHeight) * 100;
+  
+      // Show scroll circle only after some scroll
+      if (scrollTop > 100) {
+        scrollPercent.classList.add("active");
+      } else {
+        scrollPercent.classList.remove("active");
+      }
+  
+      setProgress(scrollPercentValue);
+    });
+  });
+
+  //Go to btn
+  document.addEventListener("DOMContentLoaded", function () {
+    const scrollTopBtn = document.getElementById("btnScrollTop");
+  
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 150) {
+        scrollTopBtn.classList.remove("d-none");
+      } else {
+        scrollTopBtn.classList.add("d-none");
+      }
+    });
+  
+    scrollTopBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  });
+
+  //switzeland main crousel
+  $(document).ready(function(){
+    $("#iccCarousel").owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 1000,
+      smartSpeed: 800,
+      nav: false,
+      dots: false,
+      responsive: {
+        0: { items: 1 },
+        768: { items: 1 }
+      }
+    });
+  });
