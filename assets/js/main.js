5dc211982ebff3d0f7f030e4fa633f739d472fa3
Solvebytez <githubsolvebytez@gmail.com>
2025-06-25 12:23:53 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -160,28 +160,23 @@ $(document).ready(function () {
         }
     });
 });
-$(document).ready(function () {
-    $('#carousel-publica2').owlCarousel({
-        loop: true,
-        margin: 20,
-        nav: true,
-        items:4,
-        navText: [
-            '<i class="fa fa-angle-left"></i>',
-            '<i class="fa fa-angle-right"></i>'
-        ],
-        dots: false,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        smartSpeed: 800,
-        responsive: {
-            0: { items: 1 },
-            576: { items: 2 },
-            768: { items: 3 },
-            992: { items: 4 }
-        }
+ $(document).ready(function(){
+    $("#network-slider-2").owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      responsive: {
+        0: { items: 1 },
+        576: { items: 2 },
+        768: { items: 3 },
+        992: { items: 4 },
+        1200: { items: 5 }
+      }
     });
-});
+  });
 //Scroll Percentage
 document.addEventListener("DOMContentLoaded", function () {
   const scrollPercent = document.querySelector(".scroll-count-percent");
