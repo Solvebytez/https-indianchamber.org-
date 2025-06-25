e0e966d78788004f526fe40c9a6cc8b5e6699d77
Solvebytez <githubsolvebytez@gmail.com>
2025-06-25 12:00:33 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,34 @@ $(document).ready(function () {
         autoplay: true
     });
 });
+  $(document).ready(function(){
+    $(".carousel-event2").owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        576: {
+          items: 2
+        },
+        768: {
+          items: 3
+        },
+        992: {
+          items: 4
+        },
+        1200: {
+          items: 5
+        }
+      }
+    });
+  });
 // Testimonials Slider
 $(document).ready(function () {
     $("#Icc-Pupblication").owlCarousel({
